24fb42a768d79acd242229c0a1cca0d9757105c2
tumonulo <tumonulodev@gmail.com>
2025-12-21 19:41:33 +0100

    Refactor rank structure to use objects for banners and roles, enhancing readability and maintainability

--- commands/slash/rank.js
+++ commands/slash/rank.js
@@ -1,14 +1,13 @@
 const multiplierModes = require("../../json/multiplier_modes.json")
 const {
-  ActionRowBuilder,
   ContainerBuilder,
   TextDisplayBuilder,
-  MediaComponentBuilder,
+  MediaGalleryBuilder,
+  MediaGalleryItemBuilder,
   ThumbnailBuilder,
   SectionBuilder,
   SeparatorBuilder,
-  MessageFlags,
-  EmbedBuilder
+  MessageFlags
 } = require('discord.js')
 const ranks = require("../../consts/ranks.js")
 
@@ -54,8 +53,8 @@ async run(client, int, tools) {
 
     const levelRole = levelRoles[0] || null
     const levelRoleId = levelRole?.id
-    const rank = ranks.find(r => r.roles.some(role => role.role_id === levelRoleId)) || null
-    const role = rank.roles.find(r => r.role_id === levelRoleId) || null
+    const rank = ranks.find(r => r.roles.some(role => role.id === levelRoleId)) || null
+    const role = rank.roles.find(r => r.id === levelRoleId) || null
 
     let remaining = levelData.xpRequired - xp
     let levelPercent = maxLevel ? 100 : (xp - levelData.previousLevel) / (levelData.xpRequired - levelData.previousLevel) * 100
@@ -71,7 +70,7 @@ async run(client, int, tools) {
     let estimatedMax = Math.ceil(remaining / (db.settings.gain.max * (multiplier || multiplierData.role)))
 
     // estimated number of messages to level up
-    let estimatedRange = (estimatedMax == estimatedMin) ? `${tools.commafy(estimatedMax)} ${tools.extraS("message", estimatedMax)}` : `${tools.commafy(estimatedMax)}-${tools.commafy(estimatedMin)} messages`
+    let estimatedRange = (estimatedMax == estimatedMin) ? `${tools.commafy(estimatedMax)} ${tools.extraS("mensaje", estimatedMax)}` : `${tools.commafy(estimatedMax)}-${tools.commafy(estimatedMin)} mensajes`
 
     // xp required to level up
     let nextLevelXP = (db.settings.rankCard.relativeLevel ? `${tools.commafy(xp - levelData.previousLevel)}/${tools.commafy(levelData.xpRequired - levelData.previousLevel)}` : `${tools.commafy(levelData.xpRequired)}`) + ` (${tools.commafy(remaining)} more)`
@@ -80,51 +79,66 @@ async run(client, int, tools) {
     if (cardCol == -1) cardCol = null
 
     let memberAvatar = member.displayAvatarURL()
-    let memberColor = cardCol || member.displayColor || await member.user.fetch().then(x => x.accentColor)
+    let foundCooldown = currentXP.cooldown || 0
+    let cooldown = foundCooldown > Date.now() ? tools.timestamp(foundCooldown - Date.now()) : "Sin Cooldown!"
 
-    let embed = tools.createEmbed({
-        author: { name: member.user.displayName, iconURL: memberAvatar },
-        color: memberColor,
-        footer: maxLevel ? progressBar : ((estimatedMin == Infinity || estimatedMin < 0) ? "You are unable to gain XP!" : `${progressBar}\n${estimatedRange} to go!`),
-        fields: [
-            { name: "✨ XP", value: `${tools.commafy(xp)} (lv. ${levelData.level})`, inline: true },
-            { name: "⏩ Next level", value: !maxLevel ? nextLevelXP : "Max level! Woah!", inline: true },
-        ]
-    })
-    let embed = new ContainerBuilder()
-
-    if (!db.settings.rankCard.hideCooldown) {
-        let foundCooldown = currentXP.cooldown || 0
-        let cooldown = foundCooldown > Date.now() ? tools.timestamp(foundCooldown - Date.now()) : "None!"
-        embed.addFields([{ name: "🕓 Cooldown", value: cooldown, inline: true }])
-    }
+    let container = new ContainerBuilder()
+        .setAccentColor(parseInt(rank?.color?.replace('#', ''), 16) || 3447003)
+        .addMediaGalleryComponents([
+            new MediaGalleryBuilder()
+                .setId(0)
+                .addItems([
+                    new MediaGalleryItemBuilder()
+                        .setURL(rank?.banner?.url)
+                        .setDescription(rank?.banner?.alt)
+                ])
+        ])
+        .addSeparatorComponents(new SeparatorBuilder())
+        .addSectionComponents(new SectionBuilder()
+            .addTextDisplayComponents(
+                new TextDisplayBuilder().setContent([
+                    `## ${role.emoji} <@&${role.id}>`,
+                    `### <:XP:1452305794136543263> XP`,
+                    `${tools.commafy(xp)} (lvl ${levelData.level})`,
+                    `### <:next_level:1452305794136543263> Siguiente Nivel`,
+                    `${nextLevelXP}`,
+                    `### <:cooldown:1452305790495887515> Cooldown`
+                    `${cooldown}`,
+                    `${progressBar}`,
+                    `${estimatedRange} para el siguiente nivel`
+                ].join('\n'))
+            )
+            .addTextDisplayComponents(
+                new ThumbnailBuilder({ media: { url: memberAvatar } })
+            )
+        )
 
     let hideMult = db.settings.hideMultipliers
 
     let multRoles = multiplierData.roleList
     let multiplierInfo = []
     if ((!hideMult || multiplierData.role == 0) && multRoles.length) {
-        let xpStr = multiplierData.role > 0 ? `${multiplierData.role}x XP` : "Cannot gain XP!"
+        let xpStr = multiplierData.role > 0 ? `${multiplierData.role}x XP` : "No se puede ganar XP!"
         let roleMultiplierStr = multRoles.length == 1 ? `${int.guild.id != multRoles[0].id ? `<@&${multRoles[0].id}>` : "Everyone"} - ${xpStr}` : `**${multRoles.length} roles** - ${xpStr}`
         multiplierInfo.push(roleMultiplierStr)
     }
 
     let multChannels = multiplierData.channelList
     if ((!hideMult || multiplierData.channel == 0) && multChannels.length && multiplierData.role > 0 && (multiplierData.role != 1 || multiplierData.channel != 1)) {
-        let chXPStr = multChannels[0].boost > 0 ? `${multiplierData.channel}x XP` : "Cannot gain XP!"
+        let chXPStr = multChannels[0].boost > 0 ? `${multiplierData.channel}x XP` : "No se puede ganar XP!"
         let chMultiplierStr = `<#${multChannels[0].id}> - ${chXPStr}` // leaving room for multiple channels, via categories or vcs or something
         multiplierInfo.push(chMultiplierStr)
-        if (multRoles.length) multiplierInfo.push(`**Total multiplier: ${multiplier}x XP** (${multiplierModes.channelStacking[multiplierData.channelStacking].toLowerCase()})`)
+        if (multRoles.length) multiplierInfo.push(`**{multiplier}x XP** (${multiplierModes.channelStacking[multiplierData.channelStacking].toLowerCase()})`)
     }
 
-    if (multiplierInfo.length) embed.addFields([{ name: "🌟 Multiplier", value: multiplierInfo.join("\n") }])
+    if (multiplierInfo.length) container.addFields([{ name: "<:buffies:1452368720608366653> Buffies", value: multiplierInfo.join("\n") }])
 
     else if (!db.settings.rewardSyncing.noManual && !db.settings.rewardSyncing.noWarning) {
         let syncCheck = tools.checkLevelRoles(int.guild.roles.cache, member.roles.cache, levelData.level, db.settings.rewards)
-        if (syncCheck.incorrect.length || syncCheck.missing.length) embed.addFields([{ name: "⚠ Note", value: `Your level roles are not properly synced! Type ${tools.commandTag("sync")} to fix this.` }])
+        if (syncCheck.incorrect.length || syncCheck.missing.length) container.addFields([{ name: "⚠ Nota", value: `¡Tus roles de nivel no están sincronizados correctamente! Escribe ${tools.commandTag("sync")} para solucionarlo.` }])
     }
 
     let isHidden = db.settings.rankCard.ephemeral || !!int.options.get("hidden")?.value
-    return int.reply({embeds: [embed], ephemeral: isHidden})
+    return int.reply({components: container, flags: MessageFlags.IsComponentsV2, allowedMentions: { parse: [] }, ephemeral: isHidden})
 
 }}
